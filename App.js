ccc3cc6ea24cc11f0cab7211ec16a9d5650be6a2
CyrusWarner <cyruswarner27@gmail.com>
2021-09-17 09:49:20 -0500

    Added ListItem component to display tasks for a user.

--- App.js
+++ App.js
@@ -1,45 +1,76 @@
+import React, {useState} from 'react';
 
-import React from 'react';
-
-import {SafeAreaView, StyleSheet, View, Text, TextInput, TouchableOpacity} from 'react-native';
-import ICON from "react-native-vector-icons/MaterialIcons";
-const colors = {primary: "#1f145c", white: "#fff"};
+import {
+  SafeAreaView,
+  StyleSheet,
+  View,
+  Text,
+  TextInput,
+  TouchableOpacity,
+  FlatList,
+} from 'react-native';
+import ICON from 'react-native-vector-icons/MaterialIcons';
+const colors = {primary: '#1f145c', white: '#fff'};
 
 const App = () => {
+  const [todos, setTodos] = useState([
+    {id: 1, task: 'First todo', completed: true},
+    {id: 2, task: 'Second todo', completed: false},
+  ]);
+  const ListItem = ({todo}) => {
+    return <View style={styles.listItem}>
+      <View><Text>{todo?.task}</Text></View>
+    </View>
+  }
   return (
-    <SafeAreaView style={{flex:1,backgroundColor: colors.white}}>
+    <SafeAreaView style={{flex: 1, backgroundColor: colors.white}}>
       <View style={styles.header}>
-        <Text style={{fontWeight: "bold", fontSize: 20, color: colors.primary}}>TODO APP</Text>
+        <Text style={{fontWeight: 'bold', fontSize: 20, color: colors.primary}}>
+          TODO APP
+        </Text>
         <ICON name="delete" size={25} color="red"></ICON>
       </View>
+      <FlatList 
+      showsVerticalScrollIndicator={false}
+      data={todos} 
+      contentContainerStyle={{padding: 20, paddingBottom: 100}}
+      renderItem={({item}) => <ListItem todo={item} />} />
       <View style={styles.footer}>
         <View style={styles.inputContainer}>
           <TextInput placeholder="Add Todo"></TextInput>
+        </View>
+        <TouchableOpacity>
+          <View style={styles.iconContainer}>
+            <ICON name="add" color={colors.white} size={30}></ICON>
+          </View>
+        </TouchableOpacity>
       </View>
-      <TouchableOpacity>
-        <View style={styles.iconContainer}>
-          <ICON name="add" color={colors.white} size={30}></ICON>
-        </View>
-      </TouchableOpacity>
-        </View>
     </SafeAreaView>
   );
 };
 
 const styles = StyleSheet.create({
+  listItem: {
+    padding: 20,
+    backgroundColor: colors.white,
+    flexDirection: 'row',
+    elevation: 12,
+    borderRadius: 7,
+    marginVertical: 10,
+  },
   header: {
     padding: 20,
-    flexDirection: "row",
+    flexDirection: 'row',
     alignItems: 'center',
     justifyContent: 'space-between',
   },
   footer: {
-    position: "absolute",
+    position: 'absolute',
     bottom: 0,
     color: colors.white,
     width: '100%',
-    flexDirection: "row",
-    alignItems: "center",
+    flexDirection: 'row',
+    alignItems: 'center',
     paddingHorizontal: 20,
   },
   inputContainer: {
@@ -50,18 +81,17 @@ const styles = StyleSheet.create({
     marginVertical: 20,
     marginRight: 20,
     borderRadius: 30,
-    paddingHorizontal: 20
+    paddingHorizontal: 20,
   },
   iconContainer: {
     height: 50,
     width: 50,
     backgroundColor: colors.primary,
-    borderRadius: 25, 
+    borderRadius: 25,
     elevation: 40,
     justifyContent: 'center',
-    alignItems: 'center'
-
-  }
-})
+    alignItems: 'center',
+  },
+});
 
 export default App;

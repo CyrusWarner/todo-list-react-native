035884aa1c699529f6c3fc6549b25aaf7744c5c5
CyrusWarner <cyruswarner27@gmail.com>
2021-09-17 08:53:32 -0500

    Added a header with title TODO APP and a delete icon.

--- App.js
+++ App.js
@@ -1,14 +1,29 @@
 
 import React from 'react';
 
-import {SafeAreaView, StyleSheet} from 'react-native';
+import {SafeAreaView, StyleSheet, View, Text} from 'react-native';
+import ICON from "react-native-vector-icons/MaterialIcons";
+const colors = {primary: "#1f145c", white: "#fff"};
 
 const App = () => {
   return (
-    <SafeAreaView></SafeAreaView>
+    <SafeAreaView style={{flex:1,backgroundColor: colors.white}}>
+      <View style={styles.header}>
+        <Text style={{fontWeight: "bold", fontSize: 20, color: colors.primary}}>TODO APP</Text>
+        <ICON name="delete" size={25} color="red"></ICON>
+      </View>
+    </SafeAreaView>
   );
 };
 
-const styles = StyleSheet.create({})
+const styles = StyleSheet.create({
+  header: {
+    padding: 20,
+    flexDirection: "row",
+    alignItems: 'center',
+    justifyContent: 'space-between',
+
+  }
+})
 
 export default App;
